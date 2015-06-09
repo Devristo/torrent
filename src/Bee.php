e36ad50a37d710b8761df930bbbb68ddd2233a2e
Chris <risto@live.nl>
2015-06-09 19:31:22 +0100

    Revert "Iterative decoder instead of recursive decoder"
    This reverts commit 6c341856c2d979a376a190b67896e13854ecd6f6.

--- src/Bee.php
+++ src/Bee.php
@@ -71,70 +71,107 @@ class Bee {
         else throw new \InvalidArgumentException("Input is not valid");
     }
 
-    public function decode($string){
-        $pos = 0;
 
-        $root = new Item();
-        $stack = [$root];
+    public function eatInt(&$string, &$pos){
+        // Eat the i
+        $pos++;
 
-        while($pos < strlen($string)) {
-            $item = $stack[count($stack) -1];
-
-            if($item->isFull()){
-                array_pop($stack);
-            }
-
-            if(count($stack) == 0)
+        $i = $pos;
+        while($i < strlen($string)){
+            if(ctype_digit($string[$i]) || $string[$i] == '-')
+                $i++;
+            elseif($string[$i] == 'e'){
+                $result = substr($string, $pos, $i-$pos);
+                $pos = $i+1;
+                return $result;
+            }else
                 break;
-
-            switch ($string[$pos]) {
-                case 'i':
-                    $posEnd = stripos($string, 'e', $pos);
-                    $int = (int)substr($string, $pos + 1, $posEnd - $pos - 1);
-                    $item->push($int);
-
-                    $pos = $posEnd + 1;
-                    break;
-                case 'l':
-                    $collection = new Collection();
-                    $item->push($collection);
-                    $stack[] = $collection;
-                    $pos++;
-                    break;
-
-                case 'd':
-                    $dict = new Dict();
-                    $item->push($dict);
-                    $stack[] = $dict;
-                    $pos++;
-                    break;
-
-                case 'e':
-                    array_pop($stack);
-                    $stack[count($stack)-1];
-
-                    $pos++;
-                    break;
-
-                default:
-                    $posDoubleColon = stripos($string, ':', $pos);
-                    if($posDoubleColon === false)
-                        throw new \InvalidArgumentException("Invalid string format");
-
-                    $length = (int)substr($string, $pos, $posDoubleColon-$pos);
-
-                    if($length + $posDoubleColon >= strlen($string)){
-                        throw new \InvalidArgumentException("Invalid string format");
-                    }
-
-                    $pos = $posDoubleColon + 1 + $length;
-                    $value = substr($string, $posDoubleColon + 1, $length);
-                    $item->push($value);
-            }
-
-
         }
 
-        return $root->getValue();
+        throw new \InvalidArgumentException("Invalid int format");
+    }
+
+    public function eatList(&$string, &$pos){
+        // Eat the l
+        $pos++;
+
+        $i = $pos;
+        $items = array();
+        while($i < strlen($string)){
+
+            if($string[$i] == 'e'){
+                $pos = $i+1;
+                return $items;
+            }else {
+                $items[] = $this->decode($string, $i);
+            }
+        }
+
+        throw new \InvalidArgumentException("Invalid list format");
+    }
+
+    public function eatDict(&$string, &$pos){
+        // Eat the d
+        $pos++;
+
+        $i = $pos;
+        $items = array();
+        while($i < strlen($string)){
+
+            if($string[$i] == 'e'){
+                $pos = $i+1;
+                return $items;
+            }else {
+                $key = $this->decode($string, $i);
+                $value = $this->decode($string, $i);
+
+                $items[$key] = $value;
+            }
+        }
+
+        throw new \InvalidArgumentException("Invalid dict format");
+    }
+
+    public function eatString(&$string, &$pos){
+        $i = $pos;
+        while($i < strlen($string)){
+            if(ctype_digit($string[$i]))
+                $i++;
+            elseif($string[$i] == ':'){
+                $length = (int)substr($string, $pos, $i-$pos);
+
+                if($length + $i < strlen($string)){
+                    $result = substr($string, $i+1, $length);
+                    $pos = $length + $i + 1;
+                    return $result;
+                }else break;
+
+            }else
+                break;
+        }
+        throw new \InvalidArgumentException("Invalid string format");
+    }
+
+    /**
+     * @param $string
+     * @param int $pos
+     * @return mixed
+     */
+    public function decode($string, &$pos=0){
+        while($pos < strlen($string)){
+            switch($string[$pos]){
+                case 'i':
+                    return $this->eatInt($string, $pos);
+                case 'l':
+                    return $this->eatList($string, $pos);
+                case 'd':
+                    return $this->eatDict($string, $pos);
+                default:
+                    if(ctype_digit($string[$pos]))
+                        return $this->eatString($string, $pos);
+                    else throw new \InvalidArgumentException("Invalid input format");
+
+            }
+        }
     }
 } 
